cfded1163b097f97a9c0b7032b15759c508a05f5
magico13 <magico1313@gmail.com>
2024-09-25 12:59:17 -0400

    Create template file for wifi credentials

--- src/phase0/include/wifi_creds.template.h
+++ src/phase0/include/wifi_creds.template.h
@@ -2,7 +2,8 @@
 #define WIFI_CREDS_H
 
 // Set your wifi credentials here if you want the ESP32 to
-// connect to an existing network instead of creating its own
+// connect to an existing network instead of creating its own.
+// Then rename this file to wifi_creds.h
 const char* WIFI_SSID = "";
 const char* WIFI_PASSWORD = "";
 
